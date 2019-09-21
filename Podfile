ed0ec32fe811ffa4f4ba58cd6f716b95ffe0c83a
Hardik Kothari <hardik@7peakssoftware.com>
2019-09-21 13:33:37 +0700

    Project setup

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.0'
+
+use_frameworks!
+
+def shared
+  pod 'Alamofire'
+  pod 'RxSwift'
+end
+
+target 'car4u' do
+  shared
+end
---
+++ car4u/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  car4u
+//
+//  Created by Hardik Kothari on 21/9/2562 BE.
+//  Copyright © 2562 Hardik Kothari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
